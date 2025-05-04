c940ae090191f912e7a9dd08260e9e3e6cb90271
Mehul Singh <thakurmehul492@gmail.com>
2025-05-04 16:36:34 +0000

    data types

---
+++ second.py
@@ -0,0 +1,13 @@
+# Integers and floats
+a = 5
+b = 2.5
+print(a, type(a))      # 5 <class 'int'>
+print(b, type(b))      # 2.5 <class 'float'>
+
+# Strings
+greeting = "Hello, Python!"
+print(greeting, type(greeting))  # Hello, Python! <class 'str'>
+
+# Booleans
+flag = True
+print(flag, type(flag))          # True <class 'bool'>
