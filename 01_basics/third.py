8478d02f1fde2f7559eab3469e26df13bd0963bd
Mehul Singh <thakurmehul492@gmail.com>
2025-05-04 16:39:31 +0000

    input and output

---
+++ 01_basics/third.py
@@ -0,0 +1,10 @@
+# Getting user input
+name = input("Enter your name: ") 
+ # user types, say, Alex
+print("Welcome,", name)
+
+
+
+age_str = input("Enter your age: ")
+age = int(age_str)           # convert to integer
+print("In five years you'll be", age + 5)
